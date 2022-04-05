0d96b92540c9e36363ab9f4c572c01fb7b774a72
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-05 14:38:27 +0300

    move webserver config to config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,17 @@
-import {LoggerConfig} from "../infrastructure";
+import {LoggerConfig, WebServerConfig} from "../infrastructure";
 
 const devMode = process.env.NODE_ENV !== 'production';
 
 export const config: {
-    logger: LoggerConfig
+    logger: LoggerConfig,
+    webServer: WebServerConfig
 } = {
     logger: {
         debugEnabled: devMode,
         displayDateTime: !devMode,
         displayFilePath: !devMode
     },
+    webServer: {
+        port: process.env.WEBSERVER_PORT ?? devMode ? 9000 : 80,
+    }
 }
--- src/infrastructure/webServer/webServer.ts
+++ src/infrastructure/webServer/webServer.ts
@@ -1,20 +1,24 @@
 import {default as express, Express} from 'express';
 import {Logger} from "../logger";
 
+export interface WebServerConfig {
+    port: number;
+}
+
 export class WebServer {
     private server: Express;
-    private port: number;
+    private config: WebServerConfig;
     private logger: Logger;
 
-    constructor({ port, logger}: { port: number, logger: Logger}) {
+    constructor({ config, logger}: { config: WebServerConfig, logger: Logger}) {
         this.server = express();
-        this.port = port;
+        this.config = config;
         this.logger = logger;
     }
 
     public start() {
-        this.server.listen(this.port, () => {
-            this.logger.info(`Listening on port '${this.port}'`);
+        this.server.listen(this.config.port, () => {
+            this.logger.info(`Listening on port '${this.config.port}'`);
         }).on('error', (e: Error) => {
             this.logger.error('Error listening: ', e)
         });
