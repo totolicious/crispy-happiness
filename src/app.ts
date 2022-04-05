084e5084f81a652ad30d4b02963e5945e0dc8342
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-05 14:33:18 +0300

    add basic app file

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log(123);
+import {Logger, WebServer} from "./infrastructure";
+import {config} from "./config";
+
+const logger = new Logger(config.logger);
+
+const webServer = new WebServer({ port: 9000, logger });
+webServer.start();
