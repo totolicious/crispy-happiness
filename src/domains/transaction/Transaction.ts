dce699e04f4e7ac9d8094f0ca44781e463ea7708
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 15:53:22 +0300

    add transaction typeorm entity to domains

---
+++ src/domains/transaction/Transaction.ts
@@ -0,0 +1,20 @@
+import {Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {Client} from "..";
+
+@Entity()
+export class Transaction {
+    @PrimaryGeneratedColumn()
+    public id!: number;
+
+    @Column()
+    public date!: string;
+
+    @Column("float")
+    public amount!: number;
+
+    @Column()
+    public currency!: string;
+
+    @ManyToOne(() => Client)
+    public clientId!: number;
+}
