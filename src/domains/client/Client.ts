e01cdc2678a6ee9a8d0f1b4125ee0ff42e7f436d
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 15:53:13 +0300

    add client typeorm entity to domains

---
+++ src/domains/client/Client.ts
@@ -0,0 +1,10 @@
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+
+@Entity()
+export class Client {
+    @PrimaryGeneratedColumn()
+    public id!: number;
+
+    @Column({ nullable: true, type: 'float' })
+    public commissionPercent?: number;
+}
