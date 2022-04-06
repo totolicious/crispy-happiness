a629dcddbfab3319367e88bca0d5354eeb70be07
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 15:53:51 +0300

    connect to the database

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import {Logger, WebServer} from "./infrastructure";
 import {config} from "./config";
 import { getTransactionsRouter } from "./infrastructure/endpoints/transactions/commission/commission";
 import {addYupMethods} from "./utils";
+import {initDb} from "./infrastructure/db/dataSource";
 
 
 const init = async () => {
@@ -11,15 +12,13 @@ const init = async () => {
     // initialize application logger
     const logger = new Logger(config.logger);
 
-    // create the web server
+    // connect to the database
+    await initDb({ logger, dataSourceOptions: config.db });
+
+    // create the web server, add the endpoints, start the server
     const webServer = new WebServer({ config: config.webServer, logger });
-
-    // add endpoints to web server
     webServer.addRouter(getTransactionsRouter({ logger }));
-
-    // start the web server
     await webServer.start();
 }
 
 void init();
-
