d1a1ae04f32ecfa968b7d0b7f27cbf44144deddf
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 14:06:31 +0300

    add isValidStringDate validation method

---
+++ src/utils/validation/methods/isValidStringDate/isValidStringDate.ts
@@ -0,0 +1,7 @@
+import {isValid, parse} from "date-fns";
+import { isString } from "lodash";
+
+export const isValidStringDate = (stringDate: any) =>
+  isString(stringDate) && isValid(parse(stringDate, "yyyy-MM-dd", new Date()));
+
+
