f40a6b14562427be9c0ebd9c1b5df8c308190c1c
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 14:08:50 +0300

    add route to web server

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import {Logger, WebServer} from "./infrastructure";
 import {config} from "./config";
+import { getTransactionsRouter } from "./infrastructure/endpoints/transactions/commission/commission";
+import {addYupMethods} from "./utils/validation/addYupMethods";
+
 
 const init = async () => {
+    // create and use the custom validators
+    addYupMethods();
+
+    // initialize application logger
     const logger = new Logger(config.logger);
 
+    // create the web server
     const webServer = new WebServer({ config: config.webServer, logger });
+
+    // add endpoints to web server
+    webServer.addRouter(getTransactionsRouter({ logger }));
+
+    // start the web server
     await webServer.start();
-
-
 }
 
 void init();
+
