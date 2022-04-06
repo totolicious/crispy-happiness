30985295812940b554a8696dbea4dc1e2c6d774e
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 16:05:43 +0300

    use dataSource to find the client in commission endpoint

--- src/infrastructure/endpoints/transactions/commission/commission.ts
+++ src/infrastructure/endpoints/transactions/commission/commission.ts
@@ -2,17 +2,35 @@
 import {Router} from "express";
 import { Logger } from "../../../logger";
 import {validateTransactionInput} from "./validateTransactionInput";
+import {Client} from "../../../../domains";
+import {DataSource} from "typeorm";
 
 export interface TransactionsRouterConfig {
     logger: Logger;
+    dataSource: DataSource;
 }
 
-export const getTransactionsRouter = ({}: TransactionsRouterConfig) => {
+export const getTransactionsRouter = ({ logger, dataSource }: TransactionsRouterConfig) => {
   const router = Router();
 
   router.post('/transactions/commission', async (req, res) => {
       const transaction = req.body;
       const { error } = await validateTransactionInput(transaction);
+
+      const client = await dataSource.getRepository(Client)
+          .findOneBy({ id: transaction.client_id });
+
+      if (!client) {
+          const error = {
+              name: 'DataError',
+              message: 'Client does not exist'
+          };
+          logger.debug(error);
+          res.status(400);
+          res.json({ error });
+          res.send();
+          return;
+      }
 
       if (error) {
           res.status(400);
