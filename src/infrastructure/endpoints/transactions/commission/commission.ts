07b7fdbae41f0d97386c34ad126a54c149c650f9
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 14:08:36 +0300

    add transactions commission calculation route

---
+++ src/infrastructure/endpoints/transactions/commission/commission.ts
@@ -0,0 +1,27 @@
+
+import {Router} from "express";
+import { Logger } from "../../../logger";
+import {validateTransactionInput} from "./validateTransactionInput";
+
+export interface TransactionsRouterConfig {
+    logger: Logger;
+}
+
+export const getTransactionsRouter = ({}: TransactionsRouterConfig) => {
+  const router = Router();
+
+  router.post('/transactions/commission', async (req, res) => {
+      const transaction = req.body;
+
+      const { error } = await validateTransactionInput(transaction);
+
+      if (error) {
+          res.status(400);
+          res.json({ error });
+          res.send();
+          return;
+      }
+  });
+
+  return router;
+}
