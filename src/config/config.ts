cc943086b3cb07580581954a3c93cf8f26004c9b
Gabriel Totoliciu <gabriel@totoliciu.ro>
2022-04-06 15:52:49 +0300

    add typeorm config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,28 @@
 import {LoggerConfig, WebServerConfig} from "../infrastructure";
+import {DataSourceOptions} from "typeorm/data-source/DataSourceOptions";
 
-const devMode = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== 'production';
 
 export const config: {
     logger: LoggerConfig,
-    webServer: WebServerConfig
+    webServer: WebServerConfig,
+    db: DataSourceOptions,
 } = {
     logger: {
-        debugEnabled: devMode,
-        displayDateTime: !devMode,
-        displayFilePath: !devMode
+        debugEnabled: isDev,
+        displayDateTime: !isDev,
+        displayFilePath: !isDev
     },
     webServer: {
-        port: process.env.WEBSERVER_PORT ?? devMode ? 9000 : 80,
+        port: process.env.WEBSERVER_PORT ?? isDev ? 9000 : 80,
+    },
+    db: {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5439,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'crispy_happiness',
+        logging: isDev,
     }
 }
